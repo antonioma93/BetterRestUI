3e2b3cbfa4db3d6857afa379aa7e8914dd26d5a7
Antonio Massa <antonio.massa@zehus.it>
2021-06-10 16:43:28 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Massa Antonio on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
